ee7a27b320cc85b39fc81ce26f0cc46af20ab052
Nizar Noorani <nizar.noorani@gmail.com>
2013-08-02 20:56:49 -0500

    fixes dentist verification logic

--- app/helpers/dentist_verifier.rb
+++ app/helpers/dentist_verifier.rb
@@ -1,6 +1,6 @@
 class DentistVerifier
   def verify(user)
-    return true unless user.is_dentist
+    return false if user.dental_license_number.blank?
     return true if valid_test_number? user.dental_license_number
     
     url = "http://ls.tsbde.texas.gov/inc/xml/xml-dts.php?_searchform=true&lic_nbr=#{user.dental_license_number}&first_nme=#{user.first_name}&last_nme=#{user.last_name}&_search=false&sidx=fullname&rows=1"
