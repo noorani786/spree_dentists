785d62cbb4641de64cef631fcb313e7c1eaf8122
Nizar Noorani <nizar.noorani@gmail.com>
2014-02-20 10:44:51 -0600

    makes dental license number optional

--- app/models/user_decorator.rb
+++ app/models/user_decorator.rb
@@ -4,7 +4,7 @@ module Spree
     attr_accessible :dental_license_number, :dea_number, :is_dentist
             
     validates :dental_license_number, uniqueness: true, if: "dental_license_number.present?"
-    validates :dental_license_number, presence: true, if: :is_dentist
+    # validates :dental_license_number, presence: true, if: :is_dentist
     # validates :dea_number, presence: true, if: :is_dentist
     
     before_validation :unverify, if: :dental_license_number_changed?
